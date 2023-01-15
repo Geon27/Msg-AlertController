c5f16a47eb3ae6196f048fbca87e3966b3ee9577
Geon_27 <yukgeon98527@gmail.com>
2023-01-16 00:08:46 +0900

    23.01.06 - VC.swift added code

--- Msg-AlertController/ViewController.swift
+++ Msg-AlertController/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var result: UILabel!
     @IBAction func alert(_ sender: Any) {
+        // 메시지창 객체 생성
+        let alert = UIAlertController(title: "선택", message: "항목을 선택해주세요", preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "취소", style: .cancel) // 취소버튼
         
+        // 버튼을 컨트롤러에 등록
+        alert.addAction(cancel)
+        
+        // 메시지 창 실행
+        self.present(alert, animated: false)
     }
 }
