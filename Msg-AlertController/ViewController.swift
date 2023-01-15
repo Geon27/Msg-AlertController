8a7aa871c27d8c998a64fa2ca8c42c441684e841
Geon_27 <yukgeon98527@gmail.com>
2023-01-15 18:23:56 +0900

    23/01/15 - label, button added

--- Msg-AlertController/ViewController.swift
+++ Msg-AlertController/ViewController.swift
@@ -6,14 +6,9 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var result: UILabel!
+    @IBAction func alert(_ sender: Any) {
+        
     }
-
-
 }
-
