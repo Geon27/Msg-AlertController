7b655d8c64e0d4ea0845174c0ae2b09759addcc9
Geon_27 <yukgeon98527@gmail.com>
2023-01-19 00:09:08 +0900

    23/01/19 exec + stop

--- Msg-AlertController/ViewController.swift
+++ Msg-AlertController/ViewController.swift
@@ -12,12 +12,15 @@ class ViewController: UIViewController {
         // 메시지창 객체 생성
         let alert = UIAlertController(title: "선택", message: "항목을 선택해주세요", preferredStyle: .alert)
         let cancel = UIAlertAction(title: "취소", style: .cancel) // 취소버튼
-        
         let ok = UIAlertAction(title: "확인", style: .default) // 확인버튼
+        let exec = UIAlertAction(title: "실행", style: .destructive) // 실행버튼
+        let stop = UIAlertAction(title: "중지", style: .default) // 중지버튼
         
         // 버튼을 컨트롤러에 등록
         alert.addAction(cancel)
         alert.addAction(ok)
+        alert.addAction(exec)
+        alert.addAction(stop)
         
         // 메시지 창 실행
         self.present(alert, animated: false)
