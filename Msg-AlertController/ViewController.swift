8e53bde5aa2aae69ad8385d9a8104ae4cadb27c8
Geon_27 <yukgeon98527@gmail.com>
2023-01-26 06:45:16 +0900

    23/01/26 ID,PW click action added

--- Msg-AlertController/ViewController.swift
+++ Msg-AlertController/ViewController.swift
@@ -72,6 +72,14 @@ class ViewController: UIViewController {
         let cancel = UIAlertAction(title: "취소", style: .cancel)
         let ok = UIAlertAction(title: "확인", style: .default) { (_) in
             // 확인 버튼을 클릭했을때 실행할 내용
+            let loginId = alert.textFields?[0].text
+            let loginPw = alert.textFields?[1].text
+            
+            if loginId == "sqlpro" && loginPw == "1234" {
+                self.result.text = "인증되었습니다."
+            } else {
+                self.result.text = "인증에 실패하였습니다."
+            }
         }
         
         //정의된 액션 버튼 객체를 메시지창에 추가
